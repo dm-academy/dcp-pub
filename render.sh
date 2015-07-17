25ee4244064069ac82ebaf023d42e10b7b0c41da
Charles T. Betz <char@erp4it.com>
2015-07-17 08:48:27 -0500

    commit

--- render.sh
+++ render.sh
@@ -2,6 +2,11 @@
 
 #build html
 bundle exec rake book:build
+mv aitm.html index.html
+cp -R images ../aitm-pages/
+cp *.html ../aitm-pages/
+cp *.pdf ../aitm-pages/
+
 
 # build pdf
 a2x --verbose -fpdf -dbook --fop --icons --icons-dir=$HOME/opt/asciidoc/images/icons aitm.adoc

e9df9be916252333b74c1a634ec48f1925f87584
CharlesTBetz <char@erp4it.com>
2015-07-25 18:58:21 -0500

    commit

--- render.sh
+++ render.sh
@@ -26,9 +26,11 @@ cp *.pdf ../aitm-pages/
 # dependent on successfully changing directories along relative paths.
 
 # commit to repo
-git add . -A && git commit -m "commit" && git push origin master
+git add . -A && git commit -m "commit"
+## git push origin master
 
 # push to pages.
 cd ../aitm-pages
-git add . && git commit -m "commit" && git push origin gh-pages
+git add . && git commit -m "commit"
+## git push origin gh-pages
 cd ../aitm

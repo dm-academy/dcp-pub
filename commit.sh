c7345ba64062b951c8dd90282b80f9e68acaaf6f
CharlesTBetz <char@erp4it.com>
2015-09-27 19:23:39 -0500

    commit

--- commit.sh
+++ commit.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-# this really feels ugly & brittle.
-# dependent on successfully changing directories along relative paths.
-
-# commit to repo
-git pull
-git add . -A && git commit -m "commit"
-git push origin master
-
-# push to pages.
-cd ../aitm-pages
-git pull
-sleep 5
-git add . && git commit -m "commit"
-git push origin gh-pages
-cd ../aitm
+# extinct
